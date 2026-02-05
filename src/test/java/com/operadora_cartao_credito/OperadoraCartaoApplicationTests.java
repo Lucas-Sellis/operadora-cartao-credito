92deae4e13c8c9d4fc45f3b1ce03e3e708c2bf44
Lucas <lucas.sellis@gmail.com>
2026-02-05 09:09:48 -0300

    projeto com arquitetura hexagonal

---
+++ src/test/java/com/operadora_cartao_credito/OperadoraCartaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.operadora_cartao_credito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperadoraCartaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
